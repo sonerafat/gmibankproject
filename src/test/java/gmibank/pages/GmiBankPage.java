728df975d877eb3a238d6ac6a18f82de9d665c92
AArelsen <adamarelsen@gmail.com>
2020-12-07 12:31:01 +0100

    first commit

--- src/test/java/gmibank/pages/GmiBankPage.java
+++ src/test/java/gmibank/pages/GmiBankPage.java
@@ -10,7 +10,7 @@ public class GmiBankPage {
 
     public GmiBankPage() {
         PageFactory.initElements(Driver.getDriver(), this);}
-
+    //
     @FindBy(xpath = "//a[@class='d-flex align-items-center dropdown-toggle nav-link']")
     public WebElement dropDown;
 
