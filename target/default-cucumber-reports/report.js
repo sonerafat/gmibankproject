72bea926a822053fc3f3e2fa41894dd8d5ae0bbf
Mehmet Turk <turk.mehmet0627@gmail.com>
2020-12-13 23:07:38 +0100

    Ali Said beyin kodlari

--- target/default-cucumber-reports/report.js
+++ target/default-cucumber-reports/report.js
@@ -1,78 +1,11 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/US_006_UserInfo_Positive.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/PasswordChange.feature");
 formatter.feature({
-  "name": "User Info Page Test",
+  "name": "Password Strength",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@userInfoPositiveTest"
-    }
-  ]
-});
-formatter.scenarioOutline({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \"\u003cemail\u003e\" in email textbox",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.examples({
-  "name": "Negative cases for email",
-  "description": "",
-  "keyword": "Examples",
-  "rows": [
-    {
-      "cells": [
-        "email"
-      ]
-    },
-    {
-      "cells": [
-        "44321"
-      ]
-    },
-    {
-      "cells": [
-        "usergmail.com"
-      ]
-    },
-    {
-      "cells": [
-        "user@.com"
-      ]
-    },
-    {
-      "cells": [
-        "user@gmailcom"
-      ]
-    },
-    {
-      "cells": [
-        ".com@"
-      ]
-    },
-    {
-      "cells": [
-        "212.com@"
-      ]
+      "name": "@PasswordStrength"
     }
   ]
 });
@@ -105,496 +38,80 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "Click on user name and click on User info",
+  "name": "Click on user icon and click on Password",
   "keyword": "And "
 });
 formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
+  "location": "gmibank.stepdefinitions.PasswordChange.click_on_user_icon_and_click_on_Password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter \"team21User\" to the current password textbox.",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.PasswordChange.enter_to_the_current_password_textbox(java.lang.String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Testing Update email functionality",
+  "name": "Edit Password",
   "description": "",
-  "keyword": "Scenario Outline",
+  "keyword": "Scenario",
   "tags": [
     {
-      "name": "@userInfoPositiveTest"
+      "name": "@PasswordStrength"
     },
     {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
+      "name": "@edit"
     }
   ]
 });
 formatter.step({
-  "name": "Type \"44321\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Entrance of the page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on the home page",
+  "name": "Enter \"team21User\" to the New password textbox.",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.user_is_on_the_home_page()"
+  "location": "gmibank.stepdefinitions.PasswordChange.enter_to_the_New_password_textbox(java.lang.String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "sign in as \"user\"",
+  "name": "Enter \"team21User\" to the New confirmation password textbox.",
   "keyword": "And "
 });
 formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.sign_in_as(java.lang.String)"
+  "location": "gmibank.stepdefinitions.PasswordChange.enter_to_the_New_confirmation_password_textbox(java.lang.String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "Click on user name and click on User info",
+  "name": "Click on the save button.",
   "keyword": "And "
 });
 formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoPositiveTest"
-    },
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \"usergmail.com\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
+  "location": "gmibank.stepdefinitions.PasswordChange.click_on_the_save_button()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
+  "name": "There should be a Error Message",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
+  "location": "gmibank.stepdefinitions.PasswordChange.there_should_be_a_Error_Message()"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "java.lang.AssertionError: expected [true] but found [false]\r\n\tat org.testng.Assert.fail(Assert.java:97)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:969)\r\n\tat org.testng.Assert.assertTrue(Assert.java:43)\r\n\tat org.testng.Assert.assertTrue(Assert.java:53)\r\n\tat gmibank.stepdefinitions.PasswordChange.there_should_be_a_Error_Message(PasswordChange.java:52)\r\n\tat ✽.There should be a Error Message(file:///C:/Users/user/IdeaProjects/gmibankproject/src/test/resources/features/PasswordChange.feature:13)\r\n",
+  "status": "failed"
 });
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Entrance of the page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on the home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.user_is_on_the_home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "sign in as \"user\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.sign_in_as(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on user name and click on User info",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoPositiveTest"
-    },
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \"user@.com\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Entrance of the page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on the home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.user_is_on_the_home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "sign in as \"user\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.sign_in_as(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on user name and click on User info",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoPositiveTest"
-    },
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \"user@gmailcom\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Entrance of the page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on the home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.user_is_on_the_home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "sign in as \"user\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.sign_in_as(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on user name and click on User info",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoPositiveTest"
-    },
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \".com@\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Entrance of the page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on the home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.user_is_on_the_home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "sign in as \"user\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.sign_in_as(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on user name and click on User info",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_user_name_and_click_on_User_info()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Testing Update email functionality",
-  "description": "",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "name": "@userInfoPositiveTest"
-    },
-    {
-      "name": "@userInfoNegativeTest"
-    },
-    {
-      "name": "@NegativeEmail"
-    }
-  ]
-});
-formatter.step({
-  "name": "Type \"212.com@\" in email textbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.type_in_email_textbox(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Click on save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.click_on_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Verify the error message is \"This field is invalid\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "gmibank.stepdefinitions.UserInfo.verify_the_error_message_is(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
+formatter.embedding("image/png", "embedded0.png", "screenshots");
 formatter.after({
   "status": "passed"
 });
