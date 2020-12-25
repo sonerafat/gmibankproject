e8c78d0e444c770854ee090b155cfc75d8b9693f
Soner Afat <sonerafat@gmail.com>
2020-12-25 19:06:49 +0100

    12.25 first

--- src/test/java/gmibank/stepdefinitions/Hooks.java
+++ src/test/java/gmibank/stepdefinitions/Hooks.java
@@ -15,8 +15,9 @@ public class Hooks {
   }
 
   @After
+
   public void tearDown(Scenario scenario){
-    final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+   final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
     if (scenario.isFailed()) {
       scenario.attach(screenshot, "image/png","screenshots");
     }
